d05505cc9ed97405e1e6fff8202a7557dcf1e20d
Sai Santhosh <saisanthosh@Sais-MacBook-Pro.local>
2022-02-22 15:21:14 -0800

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "7bb644596a7c8eb9382e7e0c702b7102"
+    }
+}
+
+module.exports = constants;
